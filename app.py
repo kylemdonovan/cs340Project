926191e7cd45d1a2e20468561494d0115c7c0024
Kyle <donovaky@oregonstate.edu>
2023-05-25 23:51:18 -0400

    included all html files this time

--- app.py
+++ app.py
@@ -1,72 +1,101 @@
-from flask import Flask, render_template, request, json, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
-import os
 
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
 app.config['MYSQL_USER'] = 'cs340_donovaky'
-app.config['MYSQL_PASSWORD'] = '5175' #last 4 of onid
+app.config['MYSQL_PASSWORD'] = '5175'  # last 4 of onid
 app.config['MYSQL_DB'] = 'cs340_donovaky'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 mysql = MySQL(app)
 
-# Read (Retrieve) - Display all foods
-@app.route('/foods', methods=['GET'])
-def foods():
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM foods")
-    foods = cur.fetchall()
-    cur.close()
-    return render_template('foods.html', foods=foods)
 
-# Create - Add a new food
-@app.route('/foods/add', methods=['POST'])
-def add_food():
-    name = request.form['name']
-    price = request.form['price']
-    
-    cur = mysql.connection.cursor()
-    cur.execute("INSERT INTO foods (name, price) VALUES (%s, %s)", (name, price))
-    mysql.connection.commit()
-    cur.close()
-    
-    return redirect('/foods')
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Update - Edit an existing food
-@app.route('/foods/edit/<int:food_id>', methods=['GET', 'POST'])
-def edit_food(food_id):
-    cur = mysql.connection.cursor()
-    
+
+@app.route('/clients', methods=['GET', 'POST'])
+def clients():
     if request.method == 'POST':
+        # Add new client record to the database
+        name = request.form['name']
+        # Process the data and insert into the database using MySQL queries
+        # ...
+
+        return redirect(url_for('clients'))
+    else:
+        # Retrieve client records from the database
+        # ...
+
+        return render_template('clients.html', clients=clients)
+
+
+@app.route('/foods', methods=['GET', 'POST'])
+def foods():
+    if request.method == 'POST':
+        # Add new food record to the database
         name = request.form['name']
         price = request.form['price']
-        
-        cur.execute("UPDATE foods SET name = %s, price = %s WHERE id = %s", (name, price, food_id))
-        mysql.connection.commit()
-        cur.close()
-        
-        return redirect('/foods')
-    
-    cur.execute("SELECT * FROM foods WHERE id = %s", (food_id,))
-    food = cur.fetchone()
-    cur.close()
-    
-    return render_template('edit_food.html', food=food)
+        # Process the data and insert into the database using MySQL queries
+        # ...
 
-# Delete - Remove an existing food
-@app.route('/foods/delete/<int:food_id>', methods=['POST'])
-def delete_food(food_id):
-    cur = mysql.connection.cursor()
-    cur.execute("DELETE FROM foods WHERE id = %s", (food_id,))
-    mysql.connection.commit()
-    cur.close()
-    
-    return redirect('/foods')
+        return redirect(url_for('foods'))
+    else:
+        # Retrieve food records from the database
+        # ...
 
-# Listener
-if __name__ == "__main__":
+        return render_template('foods.html', foods=foods)
 
-    #Start the app on port 3000, it will be different once hosted
-    app.run(port=58610, debug=True)
+
+@app.route('/inventories', methods=['GET', 'POST'])
+def inventories():
+    if request.method == 'POST':
+        # Add new inventory record to the database
+        name = request.form['name']
+        quantity = request.form['quantity']
+        # Process the data and insert into the database using MySQL queries
+        # ...
+
+        return redirect(url_for('inventories'))
+    else:
+        # Retrieve inventory records from the database
+        # ...
+
+        return render_template('inventories.html', inventories=inventories)
+
+
+@app.route('/regions', methods=['GET', 'POST'])
+def regions():
+    if request.method == 'POST':
+        # Add new region record to the database
+        name = request.form['name']
+        # Process the data and insert into the database using MySQL queries
+        # ...
+
+        return redirect(url_for('regions'))
+    else:
+        # Retrieve region records from the database
+        # ...
+
+        return render_template('regions.html', regions=regions)
+
+
+@app.route('/sales_history', methods=['GET', 'POST'])
+def sales_history():
+    if request.method == 'POST':
+        # Add new sales history record to the database
+        # ...
+        return redirect(url_for('sales_history'))
+    else:
+        # Retrieve sales history records from the database
+        # ...
+
+        return render_template('sales_history.html', sales_history=sales_history)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
